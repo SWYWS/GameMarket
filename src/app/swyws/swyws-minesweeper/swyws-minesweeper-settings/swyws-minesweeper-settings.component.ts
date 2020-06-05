7cfa8077fc9eb47d5bbaa629b3971097a02e685b
Alexander <swyws@yandex.ru>
2020-06-06 01:56:00 +0300

    swyws ms settings comp styled

--- src/app/swyws/swyws-minesweeper/swyws-minesweeper-settings/swyws-minesweeper-settings.component.ts
+++ src/app/swyws/swyws-minesweeper/swyws-minesweeper-settings/swyws-minesweeper-settings.component.ts
@@ -7,7 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwywsMinesweeperSettingsComponent implements OnInit {
 
+  showFormForCustomSettings: boolean = false;
+  
   constructor() { }
+
+  toggleFormForCustomSettings(): void {
+    this.showFormForCustomSettings = true;
+  }
+
+  test(): void {
+    this.showFormForCustomSettings = false;
+  }
 
   ngOnInit() {
   }
