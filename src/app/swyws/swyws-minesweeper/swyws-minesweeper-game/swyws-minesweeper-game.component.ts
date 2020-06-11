2f488d8799f6c39f18b7a23e72661cebe3eca094
Alexander <swyws@yandex.ru>
2020-06-11 21:22:05 +0300

    dblclikc added

--- src/app/swyws/swyws-minesweeper/swyws-minesweeper-game/swyws-minesweeper-game.component.ts
+++ src/app/swyws/swyws-minesweeper/swyws-minesweeper-game/swyws-minesweeper-game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SwywsMinesweeperDataService } from "../swyws-minesweeper-data.service"
 
 @Component({
   selector: 'app-swyws-minesweeper-game',
@@ -6,8 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./swyws-minesweeper-game.component.css']
 })
 export class SwywsMinesweeperGameComponent implements OnInit {
+  
+  constructor(private dataService: SwywsMinesweeperDataService) { }
 
-  constructor() { }
+  makeMove(y: number, x: number): void {
+    //y - OY coord, x - OX coord
+    this.dataService.makeMove(y, x);
+  }
+
+  placeFlag(y: number, x: number): boolean {
+    this.dataService.placeFlag(y, x);
+    return false;
+  }
+
+  makeMoveDblclick(y: number, x: number): boolean {
+    this.dataService.openClosestCells(y, x);
+    return false;
+  }
 
   ngOnInit() {
   }
